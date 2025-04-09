646ccbd5e315c8f15ea547061e7e700f51677c96
bLeohr <benjamin.leohr@stu.daviess.kyschools.us>
2025-04-09 13:29:37 +0000

    yoMama

--- MultipleGroups.java
+++ MultipleGroups.java
@@ -1,1 +1,13 @@
+public class MultipleGroups implements NumberGroup
+{
+    private List<NumberGroup> groupList;
 
+    public boolean contains(int num)
+    {
+        for(NumberGroup x: groupList)
+            if(x.contains(num))
+                return true;
+
+        return false;
+    }
+}
--- Range.java
+++ Range.java
@@ -1,1 +1,21 @@
+public class Range implements NumberGroup
+{
+    private int first;
+    private int last;
 
+    public Range(int x, int y)
+    {
+        first = x;
+        last = y;
+    }
+
+    public boolean contains(int num)
+    {
+        for(int i=first; i<last; i++)
+            if(i==num)
+                return true;
+
+        return false;
+    }
+        
+}
